e3475b94aaf90cf2afcec4beefa9b809e29fcf51
Ruurd Bijlsma <ruurdbijlsma@gmail.com>
2017-12-08 12:55:28 +0100

    Day 8 ungolfed added

--- 2017/day8.js
+++ 2017/day8.js
@@ -5,3 +5,29 @@ part1 = (i, s = {}) => i.split('\n').forEach(l => s[l.split(' ')[0]] = +eval(`${
 // Execute by typing part1(`inputstringhere`)
 // Returns highest value in registry at any point
 part2 = (i, s = {}) => Math.max(...i.split('\n').map(l => s[l.split(' ')[0]] = +eval(`${s[l.split(' ')[0]] || 0}` + (eval(`${s[l.split(' ')[4]] || 0} ${l.split(' ').slice(-2).join(' ')}`) ? `${l.split(' ')[1] === 'inc' ? '+' : '-'} ${l.split(' ')[2]}` : ''))));
+
+//Part 1 in for loop
+storage = {};
+for(let line of input.split('\n')){
+  [variable, operation, value, _, conditionVariable, operator, conditionValue] = line.split(' ');
+  currentValue=storage[line.split(' ')[0]] === undefined ? 0 : storage[line.split(' ')[0]];
+  operation=`${operation === 'inc' ? '+' : '-'} ${value}`;
+  condition = `${storage[conditionVariable] || 0} ${line.split(' ').slice(-2).join(' ')}`;
+  newValue = +eval(`${currentValue}` + (eval(condition) ? operation : ''));
+  storage[variable] = newValue;
+}
+Math.max(...Object.values(storage))
+
+//Part 2 in for loop
+storage = {};
+values = []
+for(let line of input.split('\n')){
+  [variable, operation, value, _, conditionVariable, operator, conditionValue] = line.split(' ');
+  currentValue=storage[line.split(' ')[0]] === undefined ? 0 : storage[line.split(' ')[0]];
+  operation=`${operation === 'inc' ? '+' : '-'} ${value}`;
+  condition = `${storage[conditionVariable] || 0} ${line.split(' ').slice(-2).join(' ')}`;
+  newValue = +eval(`${currentValue}` + (eval(condition) ? operation : ''));
+  values.push(newValue);
+  storage[variable] = newValue;
+}
+Math.max(values)
