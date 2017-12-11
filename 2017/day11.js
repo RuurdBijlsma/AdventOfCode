0ee2b8446d7d55ee35258353c4ec5f6720e87b40
Ruurd Bijlsma <ruurdbijlsma@gmail.com>
2017-12-11 10:16:57 +0100

    improved day 11

--- 2017/day11.js
+++ 2017/day11.js
@@ -1,45 +1,22 @@
 steps = input.split(',');
 
-y = 0;
-x = 0;
-z = 0;
+directions = {
+    'n': [0, 1, -1],
+    'ne': [1, 0, -1],
+    'se': [1, -1, 0],
+    's': [0, -1, 1],
+    'sw': [-1, 0, 1],
+    'nw': [-1, 1, 0]
+}
+
+position = [0, 0, 0];
 positions = [];
 for (let step of steps) {
-    switch (step) {
-        case 'n':
-            y++;
-            z--;
-            positions.push([x, y, z]);
-            break;
-        case 'ne':
-            z--;
-            x++;
-            positions.push([x, y, z]);
-            break;
-        case 'se':
-            y--;
-            x++;
-            positions.push([x, y, z]);
-            break;
-        case 's':
-            y--;
-            z++;
-            positions.push([x, y, z]);
-            break;
-        case 'sw':
-            x--;
-            z++;
-            positions.push([x, y, z]);
-            break;
-        case 'nw':
-            y++;
-            x--;
-            positions.push([x, y, z]);
-            break;
-    }
+    position = position.map((n, i) => n + directions[step][i]);
+    positions.push([...position]);
 }
 
 distanceFromPosition = (x, y, z) => [x, y, z].reduce((a, b) => Math.abs(a) + Math.abs(b)) / 2;
 
-part1 = distanceFromPosition(x, y, z);
+part1 = distanceFromPosition(position[0], position[1], position[2]);
 part2 = Math.max(...positions.map(p => distanceFromPosition(p[0], p[1], p[2])));
