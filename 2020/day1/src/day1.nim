4bcc8edaed712ce427b4c8660a8a1023c499a2ae
Ruurd Bijlsma <ruurd@bijlsma.dev>
2020-12-01 22:17:53 +0100

    Day 1 SPEED

--- 2020/day1/src/day1.nim
+++ 2020/day1/src/day1.nim
@@ -1,19 +1,36 @@
-import strutils, sequtils, algorithm, sugar
+import strutils, sequtils, nimbench
 
-proc first*[T](s: openArray[T], pred: proc(x: T): bool {.closure.}): T
-                                                                  {.inline.} =
-  for i in 0 ..< s.len:
-    if pred(s[i]):
-        return s[i]
+proc part1(): void =
+  const input = staticRead("../input")
 
-const input = staticRead("../input")
+  const numbers = input.split("\c\n").map(parseInt)
+  const target = 2020
 
-# Part 1
-let numbers = input.split("\c\n").map(parseInt)
-const target = 2020
+  for i in 0..<numbers.len:
+    for j in i + 1..<numbers.len:
+      if numbers[i] + numbers[j] == target:
+        echo numbers[i] * numbers[j]
+        return
 
-echo product(@[numbers, numbers]).first((nums) => nums.foldl(a + b) == target).foldl(a * b)
+proc part2(): void =
+  const input = staticRead("../input")
 
-# Part 2
-echo product(@[numbers, numbers, numbers]).first((nums) => nums.foldl(a + b) == target).foldl(a * b)
+  const numbers = input.split("\c\n").map(parseInt)
+  const target = 2020
 
+  for i in 0..<numbers.len:
+    for j in i + 1..<numbers.len:
+      for k in j + 1..<numbers.len:
+        if numbers[i] + numbers[j] + numbers[k] == target:
+          echo numbers[i] * numbers[j] * numbers[k]
+          return
+
+bench(part1, m):
+  for _ in 0..m:
+    part1()
+
+bench(part2, m):
+  for _ in 0..m:
+    part2()
+
+runBenchmarks()
