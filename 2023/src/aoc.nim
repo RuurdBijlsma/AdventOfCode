7e31e50b44b917c5fc40e80d3c13982f3d904a97
Ruurd Bijlsma <ruurd@bijlsma.dev>
2023-12-11 00:37:10 +0100

    Add empty days

--- 2023/src/aoc.nim
+++ 2023/src/aoc.nim
@@ -1,7 +1,7 @@
 import nimbench
 import std/strformat
 
-from day9/main import part1, part2
+from day10/main import part1, part2
 
 when isMainModule:
   const benchmark = true
---
+++ 2023/src/day25/main.nim
@@ -0,0 +1,19 @@
+import std/strutils
+import std/strformat
+import std/tables
+import std/sequtils
+import std/math
+import std/sugar
+import std/algorithm
+
+proc part1*(): int =
+  const input = staticRead("input")
+  const lines = input.splitLines()
+  
+  return 0
+
+proc part2*(): int =
+  const input = staticRead("input")
+  const lines = input.splitLines()
+  
+  return 0
